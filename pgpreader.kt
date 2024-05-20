be8b94bb0724c9c7030be5602d91fc907f241a45
Bastiaan Welmers <bastiaan@welmers.net>
2024-05-20 13:39:40 +0200

    WIP prove DER files are produced that work

--- pgpreader.kt
+++ pgpreader.kt
@@ -11,11 +11,19 @@ import org.bouncycastle.bcpg.ECPublicBCPGKey
 import org.bouncycastle.asn1.x9.ECNamedCurveTable
 import org.bouncycastle.asn1.ASN1ObjectIdentifier
 import org.bouncycastle.crypto.ec.CustomNamedCurves
+import org.bouncycastle.jcajce.util.DefaultJcaJceHelper
 import org.bouncycastle.math.ec.ECCurve
 import org.bouncycastle.util.BigIntegers
+import org.bouncycastle.util.encoders.Hex
+import java.security.spec.ECPoint as JavaECPoint
+import java.security.spec.ECPublicKeySpec as JavaECPublicKeySpec
+import java.security.spec.ECPrivateKeySpec as JavaECPrivateKeySpec
+import java.security.spec.ECGenParameterSpec as JavaECGenParameterSpec
+import java.security.spec.ECParameterSpec as JavaECParameterSpec
 import java.io.BufferedInputStream
 import java.io.FileInputStream
 import java.io.File
+import java.io.FileOutputStream
 import java.math.BigInteger
 import java.util.HexFormat
 
@@ -65,6 +73,15 @@ internal fun usage() {
 	println("Usage: pgpreader <file.pgp> [, key_fingerprint ]")
 }
 
+internal fun getECParameterSpec(curveOID: ASN1ObjectIdentifier): JavaECParameterSpec {
+	val helper = DefaultJcaJceHelper()
+	val params = helper.createAlgorithmParameters("EC");
+
+	params.init(JavaECGenParameterSpec(ECNamedCurveTable.getName(curveOID)));
+
+	return params.getParameterSpec(JavaECParameterSpec::class.java)
+}
+
 public fun main(args: Array<String>) {
 	if (args.size < 1) return usage()
 	val file = args[0]
@@ -85,6 +102,8 @@ public fun main(args: Array<String>) {
 	if (key != null) {
 		val packet = key.privateKeyDataPacket
 		val publicKey = key.publicKeyPacket.key
+		print("Algorithm: ")
+		println(key.publicKeyPacket.algorithm)
 		if (packet is ECSecretBCPGKey && publicKey is ECPublicBCPGKey) {
 			val d = packet.x
 			print("D: ")
@@ -96,6 +115,27 @@ public fun main(args: Array<String>) {
 			println(ecPubPoint.affineXCoord.toBigInteger())
 			print("Y: ")
 			println(ecPubPoint.affineYCoord.toBigInteger())
+			val ecPubSpec = JavaECPublicKeySpec(
+				JavaECPoint(
+					ecPubPoint.affineXCoord.toBigInteger(),
+					ecPubPoint.affineYCoord.toBigInteger()
+				),
+				getECParameterSpec(curveOID)
+			)
+			val keyFactory = DefaultJcaJceHelper().createKeyFactory("EC")
+			val pubKey = keyFactory.generatePublic(ecPubSpec)
+			val ecPrivSpec = JavaECPrivateKeySpec(d, getECParameterSpec(curveOID))
+			val privateKey = keyFactory.generatePrivate(ecPrivSpec)
+			print("Pubkey: ")
+			println(Hex.toHexString(pubKey.encoded))
+			var file = FileOutputStream("pubkey.der")
+			file.write(pubKey.encoded)
+			file.close()
+			print("Private key: ")
+			println(Hex.toHexString(privateKey.encoded))
+			file = FileOutputStream("privkey.der")
+			file.write(privateKey.encoded)
+			file.close()
 		} else {
 			println("Only EC private keys are supported as of now.")
 		}
